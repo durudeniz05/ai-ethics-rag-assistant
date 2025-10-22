8df10fc2c89a7e1991e179693b9065e45da8d1cc
durudeniz05 <durudkucukoglu@gmail.com>
2025-10-22 13:34:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import streamlit as st
-
-st.set_page_config(page_title="Super Basic Test")
-st.title("Eğer bunu görüyorsan, Streamlit çalışıyor!")
-
-print("--- Minimal Test Script Çalıştı ---") # Loglara yazacak
-import google.generativeai as genai # <-- Sadece bunu ekle
+import google.generativeai as genai # <-- Sadece bu import eklendi
 
 st.set_page_config(page_title="Import Test 1")
 st.title("Import Test: google.generativeai")
-st.write("Eğer bunu görüyorsan, 'google.generativeai' importu sorunsuz.")
+st.write("Eğer bu yazıyı görüyorsanız, 'google.generativeai' importu sorunsuz.")
 
 print("--- google.generativeai import denendi ---")
-
